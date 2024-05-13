18a1a2ea7a02896b9f9dea3defe3db0f828dd7bd
exigibil <adrian.topa04@gmail.com>
2024-05-13 21:04:45 +0300

    update json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Determine the base URL based on deployment scenario
+
 const base =
   process.env.NODE_ENV === 'production'
-    ? '/goit-react-hw-03-image-finder/'
+    ? '/goit-react-hw-04-images/'
     : '/';
 
 // https://vitejs.dev/config/
